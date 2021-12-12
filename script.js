c577cc9f21f325bd8e5a0e54f7bbd116bdd1bd76
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2021-12-12 22:41:57 +0300

    It will add the male names to be displayed

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ function formRefresh() {
     document.getElementById("midd").requestFullscreen();
 }
 
+function akanNames(){
+    var dateOfBirth = document.getElementById("dob").ariaValueMax;
+    var actualDateEntered = dateOfBirth.split("-");
+    var day = parseInt(actualDateEntered[2]);
+    var month = parseInt(actualDateEntered[1]);
+    var year = parseInt(actualDateEntered[0]);
+}
+
+// Calculating the Century
+var century = (year-1)/100+1;
+var dayOfTheWeek = (century/4-2*century-1 + 5*year/4 + 26*(month+1)/10 + day % 7;
+
+//Naming and Day Display
+document.getElementById("display").innerHTML = Math.random(dayOfTheWeek);
+var masculineNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
