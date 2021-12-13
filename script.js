d06d5ca2c8bdad5e40f364561a1300c40d5cfad1
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2021-12-13 07:05:19 +0300

    It will create the alerts on failure to give input

--- script.js
+++ script.js
@@ -1,38 +1,81 @@
 // In the event of button click
 function formRefresh() {
-    document.getElementById("midd").requestFullscreen();
+  document.getElementById("midd").requestFullscreen();
 }
 
-function akanNames(){
-    var dateOfBirth = document.getElementById("dob").ariaValueMax;
-    var actualDateEntered = dateOfBirth.split("-");
-    var day = parseInt(actualDateEntered[2]);
-    var month = parseInt(actualDateEntered[1]);
-    var year = parseInt(actualDateEntered[0]);
-}
+function akanNames() {
+  var dateOfBirth = document.getElementById("dob").ariaValueMax;
+  var actualDateEntered = dateOfBirth.split("-");
+  var day = parseInt(actualDateEntered[2]);
+  var month = parseInt(actualDateEntered[1]);
+  var year = parseInt(actualDateEntered[0]);
 
-// Calculating the Century
-var century = (year-1)/100+1;
-var dayOfTheWeek = (century/4-2*century-1 + 5*year/4 + 26*(month+1)/10 + day) % 7;
+  // Calculating the Century
+  var century = (year - 1) / 100 + 1;
+  var dayOfTheWeek =
+    (century / 4 -
+      2 * century -
+      1 +
+      (5 * year) / 4 +
+      (26 * (month + 1)) / 10 +
+      day) %
+    7;
 
-//Naming and Day Display
-document.getElementById("display").innerHTML = Math.random(dayOfTheWeek);
-var masculineNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var feminineNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"];
-var gender = document.getElementById("gender").ariaValueMax;
-var daysOfTheWeek = ["Sunday", "Monday","Tuesday", "Wednesday","Thursday", "Friday", "Saturday"];
-var dayOfWeek = Math.random(dayOfTheWeek);
+  //Naming and Day Display
+  document.getElementById("display").innerHTML = Math.random(dayOfTheWeek);
+  var masculineNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  var feminineNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ];
+  var gender = document.getElementById("gender").ariaValueMax;
+  var daysOfTheWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var dayOfWeek = Math.random(dayOfTheWeek);
 
-// Validating the form and its output
-if(!isNaN(dayOfWeek)){
-    if(gender === "Male"){
-        document.getElementById("display").innerHTML = "Your Akan Name is" + masculineNames[Math.round(dayOfTheWeek)] + "and you were born on" + daysOfTheWeek[dayOfWeek];
-    }
-    else if(gender ==="Female"){
-        document.getElementById("display").innerHTML = "Your Akan Name is" + feminineNames[Math.round(dayOfTheWeek)] + "and you were born on" + daysOfTheWeek[dayOfWeek];
+  // Validating the form and its output
+  if (!isNaN(dayOfWeek)) {
+    if (gender === "Male") {
+      document.getElementById("display").innerHTML =
+        "Your Akan Name is" +
+        masculineNames[Math.round(dayOfTheWeek)] +
+        "and you were born on" +
+        daysOfTheWeek[dayOfWeek];
+    } else if (gender === "Female") {
+      document.getElementById("display").innerHTML =
+        "Your Akan Name is" +
+        feminineNames[Math.round(dayOfTheWeek)] +
+        "and you were born on" +
+        daysOfTheWeek[dayOfWeek];
     }
     else {
         alert("Enter valid dates and choose your gender!");
-        document.getElementById("display").innerHTML = "Thank You!";
+        document.getElementById("display").innerHTML = "Thank You";
     }
+  }
+  else {
+    alert("Enter valid birthday date and choose your gender!");
+    document.getElementById("display").innerHTML = "Thank You!";
+  }
+  //Refreshing the function
 }
